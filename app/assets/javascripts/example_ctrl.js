9c807dedc85ff0694fb34520defdabdf9f4145d0
Mark Richardson <mark@markeban.com>
2016-09-01 18:53:34 -0700

    turned reviews into objects

--- app/assets/javascripts/example_ctrl.js
+++ app/assets/javascripts/example_ctrl.js
@@ -1,20 +1,28 @@
 (function () {
   "use strict";
   angular.module("app").controller("exampleCtrl", function($scope) {
-      $scope.reviews = ["it was awesome", "it was terrible", "No, I think Angular is so awesome!!!"]
+      $scope.reviews = [
+        {
+          text: "I love it!",
+          rating: 5
+        },
+        {
+          text: "I hate Angular. Why so serious!",
+          rating: 1
+        },
+        {
+          text: "It's so-so",
+          rating: 3
+        }
+      ]
 
-      $scope.addReview = function(newReview) {
+      $scope.addReview = function(newReviewText, newReviewRating) {
+        var newReview = {
+          text: newReviewText,
+          rating: newReviewRating
+        };
         $scope.reviews.push(newReview);
       }
-
-
-
-
-
-
-
-
-
 
 
     window.$scope = $scope;
